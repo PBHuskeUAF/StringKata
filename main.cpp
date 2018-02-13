afb79455df46a552763f6f6456a1e1ce6732648f
develjacob <mcjacob61@gmail.com>
2018-02-12 15:56:54 -0900

    compiled and ran empty test

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 // Paul Huskee
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <string>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+using std::string;
+
+int add_string(string numbers){
+
+	return 0;
 }
 
+TEST_CASE("add_string function"){
+	REQUIRE( add_string("") == 0);
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+
+
 }
