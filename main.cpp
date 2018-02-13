2667109bbe8cff073e0cb587c8f2e07302911f6f
develjacob <mcjacob61@gmail.com>
2018-02-12 16:09:28 -0900

    can pass single integer tests

--- main.cpp
+++ main.cpp
@@ -4,17 +4,31 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include <string>
+#include <sstream>
 
 using std::string;
+using std::cout;
+using std::istringstream;
 
 int add_string(string numbers){
-
-	return 0;
+	
+	istringstream iss; 
+	iss.str(numbers);
+	
+	int sum = 0;
+	
+	iss >> sum;
+	
+	return sum;
 }
 
 TEST_CASE("add_string function"){
+
 	REQUIRE( add_string("") == 0);
 	REQUIRE( add_string("1") == 1);
-
+	REQUIRE( add_string("2") == 2);
+	REQUIRE( add_string("10") == 10);
+	REQUIRE( add_string("1013") == 1013);
+	REQUIRE( add_string("2013") == 2013);
 
 }
